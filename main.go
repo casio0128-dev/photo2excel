1b42b2170bd57e2a9f54cf689f2f87f5c03acf21
casio0128-dev <sweet.cafe0141@gmail.com>
2023-07-20 09:53:19 +0900

    並行処理の実装

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"github.com/alecthomas/kingpin/v2"
 	_ "image/jpeg"
 	"log"
+	"path/filepath"
+	"strings"
+	"sync"
 )
 
 /*
@@ -11,24 +14,61 @@ import (
 	TODO: 全体的二里ファクタ
 */
 
+var (
+	input        string
+	output       string
+	goRoutineMax int
+)
+
+func init() {
+	kingpin.Flag("image", "Please specify the image you want to convert to Excel.").Short('i').Required().StringVar(&input)
+	kingpin.Flag("output", "Please specify the output destination.").Short('o').Default(getFileName(input)).StringVar(&output)
+	kingpin.Flag("activeRoutine", "Specify the number of coroutines to run. In addition, if it is too large, the load will be applied to the PC. Default 10 coroutines").Short('r').Default("10").IntVar(&goRoutineMax)
+	if !strings.HasSuffix(filepath.Ext(output), ".xlsx") {
+		output += ".xlsx"
+	}
+	kingpin.Parse()
+}
+
 func main() {
-	colors, err := createPhotoRGBMap("./sample.jpg")
+	colors, err := createPhotoRGBMap(input)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	book, err := openExcelBook("sample.xlsx")
+	book, err := openExcelBook(output)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	routineManager := make(chan struct{}, goRoutineMax)
+	wg := &sync.WaitGroup{}
 	for x, colorRow := range colors {
-		for y, color := range colorRow {
-			fillCellColor(book, color, x+1, y+1)
-		}
+		routineManager <- struct{}{}
+		x := x
+		colorRow := colorRow
+		wg.Add(1)
+		go func(cr []string) {
+			defer wg.Done()
+			for y, color := range cr {
+				y := y
+				color := color
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					fillCellColor(book, color, x+1, y+1)
+				}()
+			}
+		}(colorRow)
 	}
+	wg.Wait()
+	if err := book.SaveAs(output); err != nil {
+		panic(err)
+	}
+}
 
-	book.SaveAs("sample.xlsx")
-
-	fmt.Println(colors[0][0])
+func getFileName(target string) string {
+	fileName := filepath.Base(target)
+	fileExt := filepath.Ext(fileName)
+	return strings.Split(fileName, fileExt)[0]
 }
